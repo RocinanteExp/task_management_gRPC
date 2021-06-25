9777b1831270b8749ed2f4e82483a76f905e38fd
Francesco Xia <s276509@studenti.polito.it>
2021-06-25 15:36:33 +0200

    fix: missing reference to wss

--- server/components/websocket.js
+++ server/components/websocket.js
@@ -5,8 +5,9 @@ const path = require("path");
 const config = require(path.join(__dirname, "..", "configuration", "config.js"));
 const loginMessagesMap = new Map();
 
+let wss = null;
 module.exports.startServer = function startServer(port = config.wss.port) {
-    const wss = new WebSocket.Server({ port });
+    wss = new WebSocket.Server({ port });
 
     wss.on("connection", (ws) => {
         //inform the newly connected client about all the users who are currently logged in the service
@@ -17,6 +18,10 @@ module.exports.startServer = function startServer(port = config.wss.port) {
 };
 
 module.exports.sendAllClients = function sendAllClients(message) {
+    if (wss === null) {
+        throw new Error("no web socket server listening (Hint: start the server)");
+    }
+
     wss.clients.forEach(function each(client) {
         client.send(JSON.stringify(message));
     });
