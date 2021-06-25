edb8167283a80c9a07f3f900dec04177133461c3
Francesco Xia <s276509@studenti.polito.it>
2021-06-25 19:04:52 +0200

    fix: change grpc port configuration

--- server/configuration/config.js
+++ server/configuration/config.js
@@ -1,14 +1,15 @@
 const config = {};
 
-config.grpc = {};
-config.grpc.host = "0.0.0.0";
-config.grpc.port = 443;
-
-
-config.rest= {};
+config.rest = {};
 config.rest.port = 3000;
 
-config.wss= {};
+config.wss = {};
+// if you change this port you also must change the port number in the react client
+// in the react client the wss port is hard coded inside App.js
 config.wss.port = 5000;
+
+config.grpc = {};
+config.grpc.host = "0.0.0.0";
+config.grpc.port = 5001;
 
 module.exports = config;
